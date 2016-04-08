051a8aee8ef9034246493527a77a25b5f968e599
Arun <praveen@planmytour.in>
2016-04-08 09:03:57 +0530

    excel upload and editing through excel is completed

--- apps/reports/lib.py
+++ apps/reports/lib.py
@@ -1,42 +1,91 @@
 import xlrd
 from datetime import datetime
+from .models import *
 
 
 def get_or_none(classmodel, **kwargs):
-    """
-        the function will get the from the Model if exist other
-        wise will return none
+	"""
+		the function will get the from the Model if exist other
+		wise will return none
 
-        @author : Arun Gopi
-        @date   : 3/4/2016
-    """
-    try:
-        return classmodel.objects.get(**kwargs)
-    except classmodel.DoesNotExist:
-        return None
+		@author : Arun Gopi
+		@date   : 3/4/2016
+	"""
+	try:
+		return classmodel.objects.get(**kwargs)
+	except classmodel.DoesNotExist:
+		return None
 
 
 def date_from_excel(d, dm):
-    """
-        return date from excel file
+	"""
+		return date from excel file
 
-        @author : Arun Gopi
-        @date   : 6/4/2016
-    """
+		@author : Arun Gopi
+		@date   : 6/4/2016
+	"""
 
-    if d:
+	if d:
 
-        try:
-            if isinstance(d, float) or isinstance(d, int):
-                year, month, day, hour, minute, sec = xlrd.xldate_as_tuple(
-                    d, dm)
-                d = "%04d-%02d-%02d" % (year, month, day)
-            elif isinstance(d, str) or isinstance(d, unicode):
-                d = datetime.strptime(d, '%d-%m-%Y %I:%M')
-            else:
-                print "ERROR : Unknown datatype", type(d)
-        except ValueError:
-            d = None
-    else:
-        d = None
-    return d
+		try:
+			if isinstance(d, float) or isinstance(d, int):
+				year, month, day, hour, minute, sec = xlrd.xldate_as_tuple(
+					d, dm)
+				d = "%04d-%02d-%02d" % (year, month, day)
+			elif isinstance(d, str) or isinstance(d, unicode):
+				d = datetime.strptime(d, '%d-%m-%Y %I:%M')
+			else:
+				print "ERROR : Unknown datatype", type(d)
+		except ValueError:
+			d = None
+	else:
+		d = None
+	return d
+
+
+def create_project_info(data):
+	"""
+		the function will 
+		create a new record in project_info Table
+
+		@author : Arun Gopi
+		@date   : 7/4/2016
+	"""
+	
+	project = ProjectInfo()
+	project.name = data['name']
+	project.description = data['description']
+	project.start_date = data['start_date']
+	project.end_date = data['end_date']
+	project.save()
+	print 'Inserted'
+	return True
+
+def update_project_info(data):
+	"""
+		the function will update
+		ProjectInfo table if you pass PK
+
+		@author : Arun Gopi
+		@date   : 7/4/2016
+	"""
+	if 'pk' in data:
+		if data['pk'] is not None:
+			project = get_or_none(ProjectInfo, pk=data['pk'])
+			if project:
+				project.name = data['name']
+				project.description = data['description']
+				project.start_date = data['start_date']
+				project.end_date = data['end_date']
+				project.save()
+				print 'Updated'
+				return True
+			else:
+				return False
+		else:
+			return False
+
+	else:
+		print "please provide pk for updating"
+		return False
+
--- apps/reports/views.py
+++ apps/reports/views.py
@@ -1,27 +1,27 @@
 from django.shortcuts import render
-from .lib import get_or_none,date_from_excel
+from .lib import *
 from wkhtmltopdf.views import PDFTemplateResponse
 from apps.reports.models import ProjectInfo
 from django.http import Http404
 import xlwt
 import xlrd
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.utils.html import strip_tags
 from django.contrib import messages
 
 
 def insertcolx():
 	"""
-		Excel row/colum style for excel
-		@Author  : Arun Gopi
-		@date    : 3/4/2016
+			Excel row/colum style for excel
+			@Author  : Arun Gopi
+			@date    : 3/4/2016
 	"""
-	
+
 	style = xlwt.XFStyle()
 	borders = xlwt.Borders()
-	borders.bottom =borders.top= borders.left\
-	 = borders.right = xlwt.Borders.THIN
+	borders.bottom = borders.top = borders.left\
+		= borders.right = xlwt.Borders.THIN
 	borders.top = xlwt.Borders.THIN
 	style.borders = borders
 	style.pattern.pattern = 26
@@ -30,103 +30,121 @@ def insertcolx():
 	style.protection.formula_hidden = False
 	return style
 
+
 def upload_file(request):
 	"""
-		The view use for upload files
+			The view use for upload files
 
-		@Author : Arun Gopi
-		@date   : 6/4/2016
+			@Author : Arun Gopi
+			@date   : 6/4/2016
 	"""
 	sheetname = 'ProjectInfo'
-
+	return_data = {}
 
 	if 'project_info_excel' in request.FILES:
-		import pdb; pdb.set_trace()
-		upload_excel=request.FILES['project_info_excel'].read()
+
+		upload_excel = request.FILES['project_info_excel'].read()
 		try:
-			work_book=xlrd.open_workbook(file_contents=upload_excel)
+			work_book = xlrd.open_workbook(file_contents=upload_excel)
 		except:
-			messages.error(request, 'You are trying to uplaod a file that is not supported. Please select the downloaded file only.')
-			return HttpResponseRedirect(reverse('upload_file', args=[]))
+			messages.error(
+				request, 'You are trying to uplaod a file that is not supported. Please select the downloaded file only.')
+			return HttpResponseRedirect(reverse('projects', args=[]))
 
 		try:
 			worksheet = work_book.sheet_by_name(sheetname)
 		except:
-			messages.error(request, 'You are trying to uplaod a wrong file. Please upload a file with sheet name ' + sheetname +'.')
-			return HttpResponseRedirect(reverse('upload_file', args=[]))
+			messages.error(
+				request, 'You are trying to uplaod a wrong file. Please upload a file with sheet name ' + sheetname + '.')
+			return HttpResponseRedirect(reverse('projects', args=[]))
 
-		rows=worksheet.nrows
-		cols=worksheet.ncols
+		rows = worksheet.nrows
+		cols = worksheet.ncols
 
-		for row in xrange(1,rows):
-			name = worksheet.cell_value(row,0)
-			description = worksheet.cell_value(row,1)
-			start_date = date_from_excel(worksheet.cell_value(row,2), work_book.datemode)
-			end_date = date_from_excel(worksheet.cell_value(row,3), work_book.datemode)
+		row = 1
+		for row in range(1,rows):
+			data = {}
+			data['name'] = worksheet.cell_value(row, 0)
+			data['description'] = worksheet.cell_value(row, 1)
 
-			print 'name  :  ',name
-			print 'description  :  ',description
-			print 'start_date  :  ',start_date
-			print 'end_date  :  ' ,end_date
-	
-	return render(request,'reports/upload.html',{}) 
+			data['start_date'] = date_from_excel(
+				worksheet.cell_value(row, 2), work_book.datemode)
+			data['end_date'] = date_from_excel(
+				worksheet.cell_value(row, 3), work_book.datemode)
+			data['pk'] = worksheet.cell_value(
+				row, 4) if worksheet.cell_value(row, 4) else None
+
+			if data['pk']:
+				update_project_info(data)
+			else:
+				create_project_info(data)
+
+		return HttpResponseRedirect(reverse('projects'))
+	return render(request, 'reports/upload.html', return_data)
 
 
-def get_project_as_pdf(request,data):
+def get_project_as_pdf(request, data):
 	""" The function will retun project info in pdf format
-		@Author  : Arun Gopi
-		@date    : 3/4/2016
+			@Author  : Arun Gopi
+			@date    : 3/4/2016
 	"""
 
-	filename= 'project_info.pdf'
-	response = PDFTemplateResponse(request=request,\
-		template= 'reports/pdf/project_info.html',\
-		filename= filename,\
-		context= data,\
-		show_content_in_browser=False,\
-		cmd_options={ 'encoding': 'utf8','quiet': True}
-	)
+	filename = 'project_info.pdf'
+	response = PDFTemplateResponse(request=request,
+								   template='reports/pdf/project_info.html',
+								   filename=filename,
+								   context=data,
+								   show_content_in_browser=False,
+								   cmd_options={
+									   'encoding': 'utf8', 'quiet': True}
+								   )
 	return response
+
 
 def get_project_as_excel(project):
 	""" The function will retun project info in excel format
-		@Author  : Arun Gopi
-		@date    : 3/4/2016
+			@Author  : Arun Gopi
+			@date    : 3/4/2016
 	"""
 
-	row_num,col_num = 0,0
+	row_num, col_num = 0, 0
 	work_book = xlwt.Workbook(encoding='utf-8')
-	work_sheet=work_book.add_sheet("ProjectInfo")
+	work_sheet = work_book.add_sheet("ProjectInfo")
 	insertcol = insertcolx()
-	lables = ["Name ", "Overview" , "Start date", "Start date"]
+	# work_sheet.col(4).hidden = True
+	lables = ["Name ", "Overview", "Start date", "Start date"]
 	for label in lables:
 		work_sheet.write(row_num, col_num, label, insertcol)
-		work_sheet.col(col_num).width=5000
-		col_num +=1
+		work_sheet.col(col_num).width = 5000
+		col_num += 1
 	row_num += 1
-	work_sheet.write(row_num, 0,str(project.name), insertcol)
-	work_sheet.write(row_num, 1, str(strip_tags(project.description)), insertcol)
-	work_sheet.write(row_num, 2, project.start_date.strftime('%d-%m-%Y %H:%M'), insertcol)
-	work_sheet.write(row_num, 3, project.end_date.strftime('%d-%m-%Y %H:%M'), insertcol)
+	work_sheet.write(row_num, 0, str(project.name), insertcol)
+	work_sheet.write(
+		row_num, 1, str(strip_tags(project.description)), insertcol)
+	work_sheet.write(
+		row_num, 2, project.start_date.strftime('%d-%m-%Y %H:%M'), insertcol)
+	work_sheet.write(
+		row_num, 3, project.end_date.strftime('%d-%m-%Y %H:%M'), insertcol)
+	work_sheet.write(
+		row_num, 4, project.pk, insertcol)
 
 	response = HttpResponse(content_type='application/ms-excel')
 	file_name = "ProjectInfo"+'.xls'
 	response['Content-Disposition'] = 'attachment; filename="'+file_name+'"'
 	work_book.save(response)
 	return response
-	   
 
 
-def projects(request,project_id=None,download=None):
+def projects(request, project_id=None, download=None):
 	"""
-		The view use for get the project list or project info as PDF or excel
-		@Author : Arun Gopi
-		@Date   : 3/4/2016
+			The view use for get the project list or project info as PDF or excel
+			@Author : Arun Gopi
+			@Date   : 3/4/2016
 	"""
 
 	data = {}
 	if project_id:
-		project = get_or_none(ProjectInfo,pk = project_id)
+		project = get_or_none(ProjectInfo, pk=project_id)
 		if project:
 			if download == 'pdf':
 				data['project'] = project
@@ -140,6 +158,5 @@ def projects(request,project_id=None,download=None):
 		else:
 			raise Http404
 	else:
-		projects = ProjectInfo.objects.filter(is_deleted = False)
-		return render(request,'reports/projects.html',{'projects':projects}) 
-
+		projects = ProjectInfo.objects.filter(is_deleted=False)
+		return render(request, 'reports/projects.html', {'projects': projects})
