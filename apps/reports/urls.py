24ed7d78c0c0f49e4c9aea91986c17cf741a6c91
Arun <praveen@planmytour.in>
2016-04-09 08:13:40 +0530

    pointed root url to /project

--- apps/reports/urls.py
+++ apps/reports/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
 
    url(r'^pdf/project-info/(?P<project_id>[0-9]+)/(?P<download>[\w-]+)/$',views.projects,name='pdf_project_info'),
    url(r'^projects/$',views.projects,name='projects'),
+   url(r'^$',views.projects,name='projects'),
    url(r'^upload/excel$',views.upload_file,name='upload_file'),
   
 )
