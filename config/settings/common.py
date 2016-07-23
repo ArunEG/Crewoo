6934f1dc8e8ac147d2bcbce56c3caf229778929c
Arun Gopi <arungopi@Aruns-MacBook-Pro.local>
2016-07-23 21:27:45 +0530

    Added all hostes

--- config/settings/common.py
+++ config/settings/common.py
@@ -44,6 +44,7 @@ THIRD_PARTY_APPS = (
 # Apps specific for this project go here.
 LOCAL_APPS = (
     'crewoo.apps.users',  # custom users app
+    'crewoo.apps.home',
     # Your stuff: custom apps go here
 )
 
